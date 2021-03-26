224839954e0076a2e56e2d7337d947e2aa7325bb
joeldevel <christianjoel.developer@gmail.com>
2021-03-26 16:01:11 -0300

    Palindrome: applying lowerCase

--- palindrome-checker.js
+++ palindrome-checker.js
@@ -1,3 +1,10 @@
 function palindrome(str) {
+  //remove whitespaces
+  // remove non alphabetical characters
+  // transform to lowercase
+  strLowered = str.toLowerCase();
+  console.log(strLowered);
   return true;
 }
+
+palindrome("Is thIS worKinG?");
