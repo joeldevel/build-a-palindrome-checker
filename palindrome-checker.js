aa0cfb931852df06f8949a21f29c0795861f802f
joeldevel <christianjoel.developer@gmail.com>
2021-03-26 17:27:28 -0300

    Function works on example.

--- palindrome-checker.js
+++ palindrome-checker.js
@@ -1,10 +1,30 @@
 function palindrome(str) {
-  //remove whitespaces
   // remove non alphabetical characters
+  str = str.replace(/[^a-z0-9+]+/gi, '');
+  //remove whitespaces
+  let strStripped = str.replace(/\s+/g, '');
   // transform to lowercase
-  strLowered = str.toLowerCase();
-  console.log(strLowered);
-  return true;
-}
+  strLowered = strStripped.toLowerCase();
+  // console.log(strLowered);
 
-palindrome("Is thIS worKinG?");
+  const length = strLowered.length;
+  // console.log('lenght OK? ', length ==13);
+
+  let isPalindrome = true;
+  let i = 0;
+  let range = length/2;
+  while( i < range && isPalindrome) {
+    if( strLowered[i] === strLowered[length-1-i]) {
+      isPalindrome = true;
+    }
+    else {
+      isPalindrome = false;
+    }
+    i++;
+  }
+  return isPalindrome;
+}
+console.log(
+
+  palindrome("#bananab#")
+)
